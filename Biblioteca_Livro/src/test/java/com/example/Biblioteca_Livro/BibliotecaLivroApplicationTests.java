70f68e7e011ab299ef3c26eea9832066cb8bd2ae
Davimazoti <davilmazoti07@gmail.com>
2025-04-10 14:08:59 -0300

    Teste

---
+++ Biblioteca_Livro/src/test/java/com/example/Biblioteca_Livro/BibliotecaLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Biblioteca_Livro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
