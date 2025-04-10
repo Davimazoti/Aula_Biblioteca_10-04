e9b58162c23750018acb05e562b85ce5a2005641
Davimazoti <davilmazoti07@gmail.com>
2025-04-10 17:30:52 -0300

    Envio_Config

--- Biblioteca_Livro/src/main/java/com/example/Biblioteca_Livro/Controller/EmprestimoController.java
+++ Biblioteca_Livro/src/main/java/com/example/Biblioteca_Livro/Controller/EmprestimoController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/emprestimos")
+@RequestMapping("/emprestimo")
 public class EmprestimoController {
 
     @Autowired
